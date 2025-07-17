be57b53d8653ba607d642952c4857009f0f7115f
Эдуард <140618589+Eduard-Iakovlev@users.noreply.github.com>
2025-07-17 18:30:04 +0700

    Delete build directory

--- build/CMakeFiles/29_Divide_Two_Integers.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for 29_Divide_Two_Integers.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Eduard/Programms/CPP/LeetCode/29. Divide Two Integers
-# Build directory: /home/Eduard/Programms/CPP/LeetCode/29. Divide Two Integers/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
